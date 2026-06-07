754f50e20a75e56acd03260c942ec251836a74f7
Nassima <lallanassimabouchta@gmail.com>
2026-06-07 18:32:15 +0100

    getting started

---
+++ TP1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{  /*int x;
+    cout << "entrer un nombre entier : ";
+    cin >> x;
+    cout << "le carre de " << x << " est " << x*x ;
+    return 0;
+
+    char c='m', d=25 , e;
+    int i=42,j;
+    float r=678.9, s;
+    j=c;
+    cout << j << endl;
+    j=r;
+    cout << j << endl;
+    s=d;
+    cout << s << endl;
+    e=i;
+    cout << e << endl;
+  */
+
+
+
+  
+    return 0;
+    
+
+
+}
+
+
+
